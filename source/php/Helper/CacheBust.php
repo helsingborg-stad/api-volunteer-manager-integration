762a4c8c75865a6f313b64bcb664ab9b704b647e
Nikolas Ramstedt <nikolas.ramstedt@helsingborg.se>
2023-04-14 07:28:26 +0200

    formatting

--- source/php/Helper/CacheBust.php
+++ source/php/Helper/CacheBust.php
@@ -2,39 +2,38 @@
 
 namespace APIVolunteerManagerIntegration\Helper;
 
-class CacheBust
-{
-    /**
-     * Returns the revved/cache-busted file name of an asset.
-     * @param string $name Asset name (array key) from rev-mainfest.json
-     * @return string filename of the asset (including directory above)
-     */
-    public function name($name)
-    {
-        $jsonPath = API_VOLUNTEER_MANAGER_INTEGRATION_PATH . apply_filters(
-            'APIVolunteerManagerIntegration/Helper/CacheBust/RevManifestPath',
-            'dist/manifest.json'
-        );
+class CacheBust {
+	/**
+	 * Returns the revved/cache-busted file name of an asset.
+	 *
+	 * @param string $name Asset name (array key) from rev-mainfest.json
+	 *
+	 * @return string filename of the asset (including directory above)
+	 */
+	public function name( $name ) {
+		$jsonPath = API_VOLUNTEER_MANAGER_INTEGRATION_PATH . apply_filters(
+				'APIVolunteerManagerIntegration/Helper/CacheBust/RevManifestPath',
+				'dist/manifest.json'
+			);
 
-        $revManifest = [];
-        if (file_exists($jsonPath)) {
-            $revManifest = json_decode(file_get_contents($jsonPath), true);
-        } elseif ($this->isDebug()) {
-            echo '<div style="color:red">Error: Assets not built. Go to ' . API_VOLUNTEER_MANAGER_INTEGRATION_PATH . ' and run gulp. See ' . API_VOLUNTEER_MANAGER_INTEGRATION_PATH . 'README.md for more info.</div>';
-        }
+		$revManifest = [];
+		if ( file_exists( $jsonPath ) ) {
+			$revManifest = json_decode( file_get_contents( $jsonPath ), true );
+		} elseif ( $this->isDebug() ) {
+			echo '<div style="color:red">Error: Assets not built. Go to ' . API_VOLUNTEER_MANAGER_INTEGRATION_PATH . ' and run gulp. See ' . API_VOLUNTEER_MANAGER_INTEGRATION_PATH . 'README.md for more info.</div>';
+		}
 
-        if (!isset($revManifest[$name])) {
-            return;
-        }
+		if ( ! isset( $revManifest[ $name ] ) ) {
+			return;
+		}
 
-        return $revManifest[$name];
-    }
+		return $revManifest[ $name ];
+	}
 
-    /**
-     * Check if debug mode, Remove constant dependency in tests.
-     */
-    public function isDebug()
-    {
-        return defined('WP_DEBUG') && WP_DEBUG;
-    }
+	/**
+	 * Check if debug mode, Remove constant dependency in tests.
+	 */
+	public function isDebug() {
+		return defined( 'WP_DEBUG' ) && WP_DEBUG;
+	}
 }
