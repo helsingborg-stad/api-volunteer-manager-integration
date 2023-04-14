8157ea1592ca513f9270554355f9d4e5afa6c304
Nikolas Ramstedt <nikolas.ramstedt@helsingborg.se>
2023-04-14 07:26:02 +0200

    rm tests

---
+++ source/tests/php/ExampleUnitTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace APIVolunteerManagerIntegration;
+
+use PluginTestCase\PluginTestCase;
+
+class ExampleUnitTest extends PluginTestCase {
+	public function testUnitTestWorks() {
+		self::assertNotFalse( true );
+	}
+}
