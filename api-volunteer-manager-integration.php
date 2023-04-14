cd4c10502bf7f8a15dd2055d2333f77cf3671981
Nikolas Ramstedt <nikolas.ramstedt@helsingborg.se>
2023-04-14 07:35:29 +0200

    init with mock json

--- api-volunteer-manager-integration.php
+++ api-volunteer-manager-integration.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Plugin Name:       API Volunteer Manager Integration
+ * Plugin Name:       API Volunteer VirtualQuery Integration
  * Plugin URI:        https://github.com/helsingborg-stad/api-volunteer-manager-integration
  * Description:       Frontend integration for api-volunteer-manager.
  * Version:           1.0.0
@@ -13,17 +13,27 @@
  * Domain Path:       /languages
  */
 
- // Protect agains direct file access
-if (! defined('WPINC')) {
-    die;
+use APIVolunteerManagerIntegration\App;
+use APIVolunteerManagerIntegration\Services\WpRest\Local\LocalWpRestClient;
+
+// Protect against direct file access
+if ( ! defined( 'WPINC' ) ) {
+	die();
 }
 
-define('API_VOLUNTEER_MANAGER_INTEGRATION_PATH', plugin_dir_path(__FILE__));
-define('API_VOLUNTEER_MANAGER_INTEGRATION_URL', plugins_url('', __FILE__));
-define('API_VOLUNTEER_MANAGER_INTEGRATION_TEMPLATE_PATH', API_VOLUNTEER_MANAGER_INTEGRATION_PATH . 'templates/');
-define('API_VOLUNTEER_MANAGER_INTEGRATION_TEXT_DOMAIN', 'api-volunteer-manager-integration');
+define( 'API_VOLUNTEER_MANAGER_INTEGRATION_PATH', plugin_dir_path( __FILE__ ) );
+define( 'API_VOLUNTEER_MANAGER_INTEGRATION_URL', plugins_url( '', __FILE__ ) );
+define(
+	'API_VOLUNTEER_MANAGER_INTEGRATION_TEMPLATE_PATH',
+	API_VOLUNTEER_MANAGER_INTEGRATION_PATH . 'templates/'
+);
+define( 'API_VOLUNTEER_MANAGER_INTEGRATION_TEXT_DOMAIN', 'api-volunteer-manager-integration' );
 
-load_plugin_textdomain(API_VOLUNTEER_MANAGER_INTEGRATION_TEXT_DOMAIN, false, API_VOLUNTEER_MANAGER_INTEGRATION_PATH . '/languages');
+load_plugin_textdomain(
+	API_VOLUNTEER_MANAGER_INTEGRATION_TEXT_DOMAIN,
+	false,
+	API_VOLUNTEER_MANAGER_INTEGRATION_PATH . '/languages'
+);
 
 require_once API_VOLUNTEER_MANAGER_INTEGRATION_PATH . 'Public.php';
 
@@ -31,15 +41,25 @@ require_once API_VOLUNTEER_MANAGER_INTEGRATION_PATH . 'Public.php';
 require __DIR__ . '/vendor/autoload.php';
 
 // Acf auto import and export
-add_action('acf/init', function () {
-    $acfExportManager = new \AcfExportManager\AcfExportManager();
-    $acfExportManager->setTextdomain('api-volunteer-manager-integration');
-    $acfExportManager->setExportFolder(API_VOLUNTEER_MANAGER_INTEGRATION_PATH . 'source/php/AcfFields/');
-    $acfExportManager->autoExport(array(
-        'api-volunteer-manager-integration-settings' => 'group_61ea7a87e8aaa' //Update with acf id here, settings view
-    ));
-    $acfExportManager->import();
-});
+add_action( 'acf/init', function () {
+	if ( class_exists( 'AcfExportManager' ) ) {
+		$acfExportManager = new AcfExportManager();
+		$acfExportManager->setTextdomain( 'api-volunteer-manager-integration' );
+		$acfExportManager->setExportFolder(
+			API_VOLUNTEER_MANAGER_INTEGRATION_PATH . 'source/php/AcfFields/'
+		);
+		$acfExportManager->autoExport( [
+			'api-volunteer-manager-integration-settings' => 'group_61ea7a87e8aaa',
+			//Update with acf id here, settings view
+		] );
+		$acfExportManager->import();
+	}
+} );
+
+$json = file_get_contents(
+	API_VOLUNTEER_MANAGER_INTEGRATION_PATH . 'local.json'
+);
+
 
 // Start application
-new APIVolunteerManagerIntegration\App();
+new App( LocalWpRestClient::createFromJson( $json ) );
--- source/php/App.php
+++ source/php/App.php
@@ -2,9 +2,13 @@
 
 namespace APIVolunteerManagerIntegration;
 
-class App
-{
-    public function __construct()
-    {
-    }
+use APIVolunteerManagerIntegration\Services\WpRest\WpRestClient;
+use APIVolunteerManagerIntegration\Virtual\Routes;
+use APIVolunteerManagerIntegration\Virtual\VirtualQuery\Query\Context\GlobalContextFactory;
+
+class App {
+	public function __construct( WpRestClient $repository ) {
+		$virtualQuery = new Routes( $repository );
+		$virtualQuery->init( new GlobalContextFactory() );
+	}
 }
