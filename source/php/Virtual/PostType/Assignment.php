b264520f6e39df09d8461b0de5a660ff1dd1bb7a
Nikolas Ramstedt <nikolas.ramstedt@helsingborg.se>
2023-04-14 07:34:29 +0200

    add virtual cpt (assignment) & add controllers

---
+++ source/php/Virtual/PostType/Assignment.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace APIVolunteerManagerIntegration\Virtual\PostType;
+
+use APIVolunteerManagerIntegration\Services\Volunteer\AssignmentService;
+use APIVolunteerManagerIntegration\Virtual\PostType\Assignment\Controller\Archive;
+use APIVolunteerManagerIntegration\Virtual\PostType\Assignment\Controller\Single;
+use APIVolunteerManagerIntegration\Virtual\VirtualQuery\Entity\VQEntity;
+use APIVolunteerManagerIntegration\Virtual\VirtualQuery\Entity\VQFromSource;
+
+class Assignment implements VQEntity {
+	const POST_TYPE = 'volunteer-assignment';
+
+	function registerEntity( VQFromSource $virtualQuery ): VQFromSource {
+		$virtualQuery
+			->fromSource( AssignmentService::class )
+			->toPostType( self::POST_TYPE )
+			->withSlug( 'volontaruppdrag' )
+			->withLabel( __( 'Volunteer Assignment', API_VOLUNTEER_MANAGER_INTEGRATION_TEXT_DOMAIN ) )
+			->withController( new Single() )
+			->withController( new Archive() );
+
+		return $virtualQuery;
+	}
+}
