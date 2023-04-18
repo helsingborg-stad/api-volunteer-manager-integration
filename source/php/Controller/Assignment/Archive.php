d1b6918938d801260d559680f00622927094ab5a
Nikolas Ramstedt <nikolas.ramstedt@helsingborg.se>
2023-04-18 14:01:16 +0200

    map post thumbnails

--- source/php/Controller/Assignment/Archive.php
+++ source/php/Controller/Assignment/Archive.php
@@ -2,6 +2,7 @@
 
 namespace APIVolunteerManagerIntegration\Controller\Assignment;
 
+use APIVolunteerManagerIntegration\Model\VolunteerAssignment;
 use APIVolunteerManagerIntegration\Virtual\VirtualQuery\Entity\PostType\Controller\VQArchiveController;
 
 class Archive extends VQArchiveController
@@ -10,6 +11,29 @@ class Archive extends VQArchiveController
 
     function archive(array $data): array
     {
+        $data['posts'] = array_map([$this, 'mapThumbnails'], $data['posts']);
+
         return $data;
+    }
+
+    function mapThumbnails(object $post): object
+    {
+        $model = $this->getModelFromPost($post);
+        if ($model->featuredImage) {
+            $post->thumbnail = [
+                'src'   => $model->featuredImage->source,
+                'alt'   => $model->featuredImage->altText,
+                'title' => $model->featuredImage->fileName,
+            ];
+        }
+
+        return $post;
+    }
+
+    function getModelFromPost(object $post): ?VolunteerAssignment
+    {
+        return isset($post->model) && $post->model instanceof VolunteerAssignment
+            ? $post->model
+            : null;
     }
 }
