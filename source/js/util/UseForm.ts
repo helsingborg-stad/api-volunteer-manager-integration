d718930fa146358a9205ab354f53f4a94c9d11b4
Nikolas Ramstedt <nikolas.ramstedt@helsingborg.se>
2023-05-31 17:13:55 +0200

    refactor: allow dynamic object creation & simplify

--- source/js/util/UseForm.ts
+++ source/js/util/UseForm.ts
@@ -1,10 +1,13 @@
-import { ChangeEvent, useState } from 'react'
+import { ChangeEvent, useCallback, useState } from 'react'
 
 function setNestedObjectValue<T>(obj: T, path: string, value: any): T {
   const keys = path.split('.')
   let current: any = obj
 
   for (let i = 0; i < keys.length - 1; i++) {
+    if (!current[keys[i]]) {
+      current[keys[i]] = {}
+    }
     current = current[keys[i]]
   }
 
@@ -20,17 +23,20 @@ interface FormProps<T> {
 export function useForm<T>({ initialState, resetState }: FormProps<T>) {
   const [formState, setFormState] = useState<T>(initialState)
 
-  const handleInputChange = (field: string) => (event: ChangeEvent<{ value: unknown }>) => {
-    setFormState((prev) => {
-      const newFormState = { ...prev }
-      setNestedObjectValue(newFormState, field, event.target.value as keyof typeof prev)
-      return newFormState
-    })
-  }
+  const handleInputChange = useCallback(
+    (field: string) => (event: ChangeEvent<{ value: unknown }>) => {
+      setFormState((prev) => {
+        const newFormState = { ...prev }
+        setNestedObjectValue(newFormState, field, event.target.value as keyof typeof prev)
+        return newFormState
+      })
+    },
+    [],
+  )
 
-  const resetForm = () => {
+  const resetForm = useCallback(() => {
     setFormState(resetState || initialState)
-  }
+  }, [resetState, initialState])
 
   return { formState, handleInputChange, resetForm }
 }
