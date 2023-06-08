365126ab48babfda0570cdffc845e07c6fecd136
Nikolas Ramstedt <nikolas.ramstedt@helsingborg.se>
2023-06-08 13:28:24 +0200

    allow accessing items by index

--- source/php/Model/Generic/Collection.php
+++ source/php/Model/Generic/Collection.php
@@ -2,14 +2,16 @@
 
 namespace APIVolunteerManagerIntegration\Model\Generic;
 
+use ArrayAccess;
 use IteratorAggregate;
 use Traversable;
 
 /**
  * @template T
  * @template-implements IteratorAggregate<int, T>
+ * @template-implements ArrayAccess<int, T>
  */
-class Collection implements IteratorAggregate
+class Collection implements IteratorAggregate, ArrayAccess
 {
     private array $items;
 
@@ -27,5 +29,50 @@ class Collection implements IteratorAggregate
     public function getIterator(): Traversable
     {
         yield from $this->items;
+    }
+
+    /**
+     * Check if offset exists
+     *
+     * @param  int  $offset
+     *
+     * @return bool
+     */
+    public function offsetExists($offset)
+    {
+        return isset($this->items[$offset]);
+    }
+
+    /**
+     * Get value at a specific offset
+     *
+     * @param  int  $offset
+     *
+     * @return T
+     */
+    public function offsetGet($offset)
+    {
+        return $this->items[$offset];
+    }
+
+    /**
+     * Set value at a specific offset
+     *
+     * @param  int  $offset
+     * @param  T  $value
+     */
+    public function offsetSet($offset, $value)
+    {
+        $this->items[$offset] = $value;
+    }
+
+    /**
+     * Unset value at a specific offset
+     *
+     * @param  int  $offset
+     */
+    public function offsetUnset($offset)
+    {
+        unset($this->items[$offset]);
     }
 }
