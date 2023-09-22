fad559db0847dd2f9f054637b26119879c96b6db
Nikolas Ramstedt <nikolas.ramstedt@helsingborg.se>
2023-09-22 19:16:38 +0200

    cleanup

--- source/views/single-vol-assignment.blade.php
+++ source/views/single-vol-assignment.blade.php
@@ -47,31 +47,41 @@
     <div>
         @if (!empty($volunteerAssignmentViewModel['signUpInfo']))
             <div class='u-margin__top--5'>
-                @include('volunteer-assignment.sign-up-info', ['viewModel' => (object) $volunteerAssignmentViewModel['signUpInfo']])
+                @include('volunteer-assignment.sign-up-info', [
+                    'viewModel' => (object) $volunteerAssignmentViewModel['signUpInfo']
+                ])
             </div>
         @endif
 
         @if (!empty($volunteerAssignmentViewModel['employerInfo']))
             <div class='u-margin__top--5'>
-                @include('volunteer-assignment.employer-info', ['viewModel' => (object) $volunteerAssignmentViewModel['employerInfo']])
+                @include('volunteer-assignment.employer-info', [
+                    'viewModel' => (object) $volunteerAssignmentViewModel['employerInfo']
+                ])
             </div>
         @endif
 
         @if (!empty($volunteerAssignmentViewModel['contactInfo']))
             <div class='u-margin__top--5'>
-                @include('volunteer-assignment.contact-info', ['viewModel' => (object) $volunteerAssignmentViewModel['contactInfo']])
+                @include('volunteer-assignment.contact-info', [
+                    'viewModel' => (object) $volunteerAssignmentViewModel['contactInfo']
+                ])
             </div>
         @endif
 
         @if (!empty($volunteerAssignmentViewModel['loginModal']))
             <div>
-                @include('volunteer-assignment.modal.login', ['viewModel' => (object) $volunteerAssignmentViewModel['loginModal']])
+                @include('volunteer-assignment.modal.login', [
+                    'viewModel' => (object) $volunteerAssignmentViewModel['loginModal']
+                ])
             </div>
         @endif
 
         @if (!empty($volunteerAssignmentViewModel['signUpModal']))
             <div>
-                @include('volunteer-assignment.modal.sign-up', ['viewModel' => (object) $volunteerAssignmentViewModel['signUpModal']])
+                @include('volunteer-assignment.modal.sign-up', [
+                    'viewModel' => (object) $volunteerAssignmentViewModel['signUpModal']
+                ])
             </div>
         @endif
     </div>
--- source/views/volunteer-assignment/employer-info.blade.php
+++ source/views/volunteer-assignment/employer-info.blade.php
@@ -25,9 +25,6 @@
 
         @endforeach
         @endcollection
-
-
     </div>
-
 </div>
 
