c6829c49499d155dfd42fc7dbc9508f5e080538a
Nikolas Ramstedt <nikolas.ramstedt@helsingborg.se>
2023-10-05 12:31:09 +0200

    add(composeEventFns, setValidity, reportValidty): event utils

--- source/js/util/event.ts
+++ source/js/util/event.ts
@@ -1,4 +1,23 @@
+export type EventFnType = (
+  e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement>,
+) => any
+
+export const composeEventFns =
+  (fns: EventFnType[]) =>
+  (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement>) =>
+    fns.map((fn) => fn(e))
+
 export const parseValue =
   (cb: (v: any) => any) =>
   (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement>) =>
     cb(e.target.value)
+
+export type ValidationCallbackType = (v: any, state: ValidityState) => string
+export const setValidity =
+  (cb: ValidationCallbackType) =>
+  (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement>) =>
+    e.target.setCustomValidity(cb(e.target.value, e.target.validity) ?? '')
+
+export const reportValidity = (
+  e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement>,
+) => e.target.value && e.target.reportValidity()
