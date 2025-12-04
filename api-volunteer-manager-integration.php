2c139858089a0eade7118b8225719273f829362c
GitHub Action <>
2025-12-04 09:24:37 +0000

    build: update package file versions to 5.1.3

--- api-volunteer-manager-integration.php
+++ api-volunteer-manager-integration.php
@@ -3,7 +3,7 @@
  * Plugin Name: API Volunteer Manager Integration
  * Plugin URI: https://github.com/helsingborg-stad/api-volunteer-manager-integration
  * Description: Frontend integration for api-volunteer-manager.
- * Version: 5.1.1
+ * Version: 5.1.3
  * Author: Nikolas Ramstedt
  * Author URI: https://github.com/helsingborg-stad
  * License: MIT
